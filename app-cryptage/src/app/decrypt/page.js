da3d742b97d6cf8b765b107a0622f5ccab381a69
Charles <charleslantiguajorge@gmail.com>
2024-11-15 00:07:36 +0100

    fix: suppression du dossier app-cryptage qui était en trop sur la branche 'brancheCharles'

--- app-cryptage/src/app/decrypt/page.js
+++
@@ -1,6 +0,0 @@
-// src/app/decrypt/page.js
-import DecryptForm from '../components/DecryptForm';
-
-export default function DecryptPage() {
-  return <DecryptForm />;
-}
--- app-cryptage/src/app/encrypt/page.js
+++
@@ -1,6 +0,0 @@
-// src/app/encrypt/page.js
-import EncryptForm from '../components/EncryptForm';
-
-export default function EncryptPage() {
-  return <EncryptForm />;
-}
