174a045eed0c58b27b2354223b379e2cab2385a4
Charles <charleslantiguajorge@gmail.com>
2024-11-18 03:21:35 +0100

    fix: correction de supabase.js

--- app-cryptage/src/lib/supabase.js
+++ app-cryptage/src/lib/supabase.js
@@ -1,10 +1,22 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+let supabase;
 
-if (!supabaseUrl || !supabaseKey) { // On a ajouté un test pour vérifier si les accès étaient présent
-  throw new Error("Il manque les codes d'accès à la base de donné Supabase ( API et URL )");
+try {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+  if (!supabaseUrl || !supabaseKey) {
+    console.warn('Les variables d\'environnement Supabase sont manquantes')
+    
+    supabase = createClient('', '')
+  } else {
+    supabase = createClient(supabaseUrl, supabaseKey)
+  }
+} catch (error) {
+  console.error('Erreur lors de l\'initialisation de Supabase:', error)
+  
+  supabase = createClient('', '')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export { supabase }
